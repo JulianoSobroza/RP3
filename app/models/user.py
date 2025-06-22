8bd44cf6254ad750573b6e736bb6e4070a09e5b6
JulianoSobroza <139268852+JulianoSobroza@users.noreply.github.com>
2025-06-22 16:40:35 -0300

    tutorial de instalacao melhorado, login de usuario corrigido

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,9 @@
 from app import db
+import uuid
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(256), nullable=False)
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    username = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,15 +1,46 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request,flash
+from flask_login import login_user, login_required, logout_user, current_user, LoginManager
+from werkzeug.security import generate_password_hash, check_password_hash
+from app.services import user_services
 
 auth_bp = Blueprint('auth', __name__)
 
-@auth_bp.route('/register')
+@auth_bp.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        name = request.form.get('name')
+
+        if user_services.get_user_by_email(email):
+            flash('Email ja cadastrado.', 'danger')
+            return render_template('register.html')
+
+        hashed_password = generate_password_hash(password)
+        user_services.create_user(email=email, password=hashed_password, name=name)
+        flash('Cadastro realizado com sucesso! Realize o login.', 'success')
+        return redirect(url_for('auth.login'))
+
     return render_template('register.html')
 
-@auth_bp.route('/login')
-def login():
-    return render_template('login.html')
 
+@auth_bp.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        user = user_services.get_user_by_email
+
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for("dashboard.dashboard"))
+        flash("Credenciais invalidas", "danger")
+
+    return render_template("login.html")
+
+
+@login_required
 @auth_bp.route('/logout')
 def logout():
+    logout_user()
     return redirect(url_for('main.index'))
