f5dd530026451205ad89edb8fbfe6728b75bda03
JulianoSobroza <139268852+JulianoSobroza@users.noreply.github.com>
2025-06-22 16:16:26 -0300

    corrigido requirements e config inicial

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "chave_secreta")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "postgresql://postgres:root@172.29.208.1/RP3")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
